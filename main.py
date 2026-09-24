a587c1167a76f1f4ef8ad85349f075e7dbda36a5
NikRait <ivantokorev9@gmail.com>
2026-09-24 18:30:53 +0300

    Fifth commit

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ print("Дата: 2026-09-10")
 
 def square(x: int) -> int:
     return x * x
+
+
 number = 7
 result = square(number)
 print(f"Число: {number}")
 print(f"Квадрат: {result}")
+
+
+# Factorial
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+n = 10
+print(f"Factorial for {n} = {factorial(5)}")  # Output: 120
