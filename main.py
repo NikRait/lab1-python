5bed3ce13b8feff48bf93653adaf78d653195f7d
NikRait <ivantokorev9@gmail.com>
2026-09-24 16:29:19 +0300

    Fourth commit

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ name = "Golev Sergey"
 print(f"Студент: {name}")
 # DATA
 print("Дата: 2026-09-10")
+
+
+def square(x: int) -> int:
+    return x * x
+number = 7
+result = square(number)
+print(f"Число: {number}")
+print(f"Квадрат: {result}")
